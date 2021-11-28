7e9e3f6ce183b64440ec9a41596902d8487d0f26
Itamar Mizrahi <48559693+itamar-mizrahi@users.noreply.github.com>
2021-11-28 14:59:17 +0200

    fix syntax

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
-import './App.css';
-import { answers } from './data/answers'; 
-import { chats } from './data/chats'; 
-import { questions } from './data/questions'; 
+import "./App.css";
+import { answers } from "./data/answers";
+import { chats } from "./data/chats";
+import { questions } from "./data/questions";
 
 // import ChatsData from './components/ChatsData';
 // import InputData from './components/InputHandle';
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 function App() {
   const [data, setData] = useState([]);
-  const [ID, setID] = useState()
-  const handleQuestions=()=>{}
-  const handleAnswers=()=>{}
-  const getData=(ID)=>{
-    chats.forEach(chat=>{
-      if (chat.chatID===ID) {
+  const [ID, setID] = useState();
+  const handleQuestions = () => {};
+  const handleAnswers = () => {};
+  const getData = (ID) => {
+    chats.forEach((chat) => {
+      if (chat.chatID === ID) {
         return chat.questions;
       }
-
-    } 
-  }
-  useEffect(()=>{
-    getData()
-  },[])
-  const handleInput=(e)=> {
+    });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  const handleInput = (e) => {
     e.preventDefault();
     getData(ID);
     return data;
-  }
+  };
   return (
     <div className="App">
       <form onSubmit={handleInput}>
-      <header className="App-header">
-        enter yout ID here:
-        <input type="text" value={ID} onChange={e=>setID(e.target.value)} placeholder="enter ID" />
-    <button type="submit">get data</button>
-
-    {/* <InputData/> */}
-    {/* <ChatsData data={data}/> */}
-
-      </header>
+        <header className="App-header">
+          enter yout ID here:
+          <input
+            type="text"
+            value={ID}
+            onChange={(e) => setID(e.target.value)}
+            placeholder="enter ID"
+          />
+          <button type="submit">get data</button>
+          {/* <InputData/> */}
+          {/* <ChatsData data={data}/> */}
+        </header>
       </form>
     </div>
   );
 }
-
 
 export default App;
