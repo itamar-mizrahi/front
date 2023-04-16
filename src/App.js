6f085295c4eadaad846493bf2b371bfb586b42e6
Itamar Mizrahi <48559693+itamar-mizrahi@users.noreply.github.com>
2023-04-16 23:05:26 +0300

    node fix

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect, useMemo } from "react";
 import "./App.css";
 import { answers } from "./data/answers";
 import { chats } from "./data/chats";
@@ -5,32 +6,37 @@ import { questions } from "./data/questions";
 
 // import ChatsData from './components/ChatsData';
 // import InputData from './components/InputHandle';
-import { useState, useEffect } from "react";
+
+const Questions = ({ questions }) => (
+  <>
+    {questions.map((question) => (
+      <li key={question.order}>
+        {question.order},{question.qid}
+      </li>
+    ))}
+  </>
+);
+
 function App() {
   const [data, setData] = useState([]);
   const [ID, setID] = useState();
   const handleQuestions = () => {};
   const handleAnswers = () => {};
-  const getData = (ID) => {
-    chats.forEach((chat) => {
-      if (chat.chatID === ID) {
+
+  const ChatData = useMemo(() => {
+    if (chats.chatID === ID) {
+      return chats.map((chat) => {
         console.log(chat.questions);
         //TODO:switch case of data by qid and type of question
-        return (chat.questions.map(question=><li>{question.order}, {question.qid}</li>));
-      }
-      else{
-
-        return (<div>Nothing found</div>);
-      }
-    });
-    return (<div>Nothing yet</div>);
-  };
-  useEffect(() => {
-    getData(ID);
+        return <Questions questions={chat.questions} />;
+      });
+    }
+    return <div>Nothing yet</div>;
   }, [ID]);
+
+  useEffect(() => {}, [ID]);
   const handleInput = (e) => {
     e.preventDefault();
-    getData(ID);
   };
   return (
     <div className="App">
@@ -49,6 +55,7 @@ function App() {
           {/* <ChatsData data={data}/> */}
         </header>
       </form>
+      {ChatData}
     </div>
   );
 }
